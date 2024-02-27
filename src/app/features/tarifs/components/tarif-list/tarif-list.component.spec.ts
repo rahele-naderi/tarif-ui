b65b98cc3b9c009c9e6150fe04ddfa832bdbaef6
Rahele Naderi <naderi.rahele2017@gmail.com>
2024-02-28 00:13:50 +0100

    add search with tests

--- src/app/features/tarifs/components/tarif-list/tarif-list.component.spec.ts
+++ src/app/features/tarifs/components/tarif-list/tarif-list.component.spec.ts
@@ -26,4 +26,64 @@ describe('TarifListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call filterList when download search input changes', () => {
+    const filterListSpy = spyOn(component, 'filterList');
+
+    component.searchDownloadControl.setValue('12');
+
+    expect(filterListSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call filterList when upload search input changes', () => {
+    const filterListSpy = spyOn(component, 'filterList');
+
+    component.searchUploadControl.setValue('6');
+
+    expect(filterListSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call filterList when price search input changes', () => {
+    const filterListSpy = spyOn(component, 'filterList');
+
+    component.searchPriceControl.setValue('123.45');
+
+    expect(filterListSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return list when download is set correctly', () => {
+    const result = component.filterList([[{ name: 'test', benefits: [], download: 12, upload: 6, price: 123 }], '12', '', '']);
+
+    expect(result).toEqual([{ name: 'test', benefits: [], download: 12, upload: 6, price: 123 }]);
+  });
+
+  it('should return list when download is not set correctly', () => {
+    const result = component.filterList([[{ name: 'test', benefits: [], download: 12, upload: 6, price: 123 }], '11', '', '']);
+
+    expect(result).toEqual([]);
+  });
+
+  it('should return list when upload is set correctly', () => {
+    const result = component.filterList([[{ name: 'test', benefits: [], download: 12, upload: 6, price: 123 }], '', '6', '']);
+
+    expect(result).toEqual([{ name: 'test', benefits: [], download: 12, upload: 6, price: 123 }]);
+  });
+
+  it('should return list when upload is not set correctly', () => {
+    const result = component.filterList([[{ name: 'test', benefits: [], download: 12, upload: 6, price: 123 }], '', '5', '']);
+
+    expect(result).toEqual([]);
+  });
+
+  it('should return list when price is set correctly', () => {
+    const result = component.filterList([[{ name: 'test', benefits: [], download: 12, upload: 6, price: 123 }], '', '', '123']);
+
+    expect(result).toEqual([{ name: 'test', benefits: [], download: 12, upload: 6, price: 123 }]);
+  });
+
+  it('should return list when price is not set correctly', () => {
+    const result = component.filterList([[{ name: 'test', benefits: [], download: 12, upload: 6, price: 123 }], '', '', '122']);
+
+    expect(result).toEqual([]);
+  });
 });
--- src/app/features/tarifs/components/tarif-list/tarif-list.component.ts
+++ src/app/features/tarifs/components/tarif-list/tarif-list.component.ts
@@ -1,19 +1,49 @@
 import { Component, LOCALE_ID } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { GetTarifsService } from '../../services/get-tarifs.service';
 import { AsyncPipe, CurrencyPipe, NgFor } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { combineLatest, debounceTime, map, merge, startWith } from 'rxjs';
+import { Tarif } from '../../models/tarif';
 
 @Component({
   selector: 'vrx-tarif-list',
   standalone: true,
-  imports: [NgFor, AsyncPipe, CurrencyPipe],
-  providers: [ { provide: LOCALE_ID, useValue: 'de-DE' } ],
+  imports: [NgFor, AsyncPipe, CurrencyPipe, ReactiveFormsModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'de-DE' }],
   templateUrl: './tarif-list.component.html',
-  styleUrl: './tarif-list.component.sass'
+  styleUrl: './tarif-list.component.sass',
 })
 export class TarifListComponent {
   tarifs$;
+  searchDownloadControl = new FormControl<string>('');
+  searchUploadControl = new FormControl<string>('');
+  searchPriceControl = new FormControl<string>('');
 
   constructor(getTarifService: GetTarifsService) {
-    this.tarifs$ = getTarifService.getTarifs();
+    this.tarifs$ = combineLatest([
+      getTarifService.getTarifs(),
+      this.searchDownloadControl.valueChanges.pipe(startWith('')),
+      this.searchUploadControl.valueChanges.pipe(startWith('')),
+      this.searchPriceControl.valueChanges.pipe(startWith('')),
+    ]).pipe(
+      takeUntilDestroyed(),
+      map((values) => this.filterList(values))
+    );
+  }
+
+  filterList(values: [Tarif[], string | null, string | null, string | null]) {
+    const [tarifs, download, upload, price] = values;
+    let result = tarifs;
+    if (download && !isNaN(+download)) {
+      result = result.filter((res) => res.download === +download);
+    }
+    if (upload && !isNaN(+upload)) {
+      result = result.filter((res) => res.upload === +upload);
+    }
+    if (price && !isNaN(+price)) {
+      result = result.filter((res) => res.price === +price);
+    }
+    return result;
   }
 }
