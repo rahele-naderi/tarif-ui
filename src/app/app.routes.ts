f11f6bbbc4955a35040883029eca4c493d719da7
Rahele Naderi <naderi.rahele2017@gmail.com>
2024-02-27 22:23:58 +0100

    add list page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'list' },
+    { path: 'list', component: ListPageComponent }
+];
